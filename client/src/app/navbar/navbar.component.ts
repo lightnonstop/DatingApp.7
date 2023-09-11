1f26e696e99e62f8803717b4bd441f33336204ef
lightnonstop <umichaeledet003@gmail.com>
2023-09-11 21:15:56 +0100

    remove unnecessary error indication

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -22,7 +22,6 @@ export class NavbarComponent {
   login() {
     this.accountService.login(this.model).subscribe({
       next: (_) => this.router.navigateByUrl('/members'),
-      error: (error) => this.toastr.error(error.error),
     });
   }
 
