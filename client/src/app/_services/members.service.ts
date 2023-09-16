ee5defdf5017096879ccccca7a4d1032bcfbc681
lightnonstop <umichaeledet003@gmail.com>
2023-09-16 09:53:40 +0100

    setup state storing for user data update

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -22,6 +23,17 @@ export class MembersService {
   }
 
   getMember(username: string) {
+    const member = this.members.find((m) => m.userName === username);
+    if (member) return of(member);
     return this.http.get<Member>(`${this.baseUrl}users/${username}`);
+  }
+
+  updateMember(member: Member) {
+    return this.http.put(`${this.baseUrl}users`, member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = { ...this.members[index], ...member };
+      })
+    );
   }
 }
