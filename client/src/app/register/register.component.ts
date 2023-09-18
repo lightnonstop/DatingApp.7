d4e0d8502ba9c7833a8bd970a412cbbcbae1c8ec
lightnonstop <umichaeledet003@gmail.com>
2023-09-18 08:33:20 +0100

    add input validators

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,28 +1,51 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
+  registerForm: FormGroup = new FormGroup({});
 
   constructor(
     private accountService: AccountService,
     private toastr: ToastrService
   ) {}
 
-  register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: (error) => this.toastr.error(error.error),
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl('', Validators.required),
     });
+  }
+
+  register() {
+    console.log(this.registerForm?.value);
+
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: (error) => {
+    //     this.toastr.error(error.error);
+    //     this.toastr.error(error.error);
+    //   },
+    // });
   }
 
   cancel() {
