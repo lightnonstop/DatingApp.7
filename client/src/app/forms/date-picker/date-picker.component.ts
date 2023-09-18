f98e76afb89c17558b6aa17728159fad78a59351
lightnonstop <umichaeledet003@gmail.com>
2023-09-18 22:20:59 +0100

    color of date picker to red

--- client/src/app/forms/date-picker/date-picker.component.ts
+++ client/src/app/forms/date-picker/date-picker.component.ts
@@ -15,7 +15,7 @@ export class DatePickerComponent implements ControlValueAccessor {
   constructor(@Self() public ngControl: NgControl) {
     this.ngControl.valueAccessor = this;
     this.bsConfig = {
-      containerClass: 'theme-orange',
+      containerClass: 'theme-red',
       dateInputFormat: 'DD MMMM YYYY',
     };
   }
