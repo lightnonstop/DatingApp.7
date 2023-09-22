6a5b7a54cec26826bdc6949f20564e125f263623
lightnonstop <umichaeledet003@gmail.com>
2023-09-22 11:16:15 +0100

    remove inherited properties

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -2,15 +2,6 @@ namespace API.Helpers
 {
     public class UserParams
     {
-        private const int MaxPageSize = 100;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 10;
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
-
         public string CurrentUsername { get; set; }
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
