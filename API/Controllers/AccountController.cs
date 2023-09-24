e5e83dc8639754cb3c4106e638ca86aaa2a41515
lightnonstop <umichaeledet003@gmail.com>
2023-09-24 16:12:01 +0100

    fix: add await keyword

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -39,7 +39,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -61,7 +61,7 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user),
+                Token = await _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
