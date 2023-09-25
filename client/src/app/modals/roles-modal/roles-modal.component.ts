4a545b85bca2eecc84a4959b56bcb5dd3bbda528
lightnonstop <umichaeledet003@gmail.com>
2023-09-25 19:25:48 +0100

    add file from angular CLI

---
+++ client/src/app/modals/roles-modal/roles-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles-modal',
+  templateUrl: './roles-modal.component.html',
+  styleUrls: ['./roles-modal.component.css']
+})
+export class RolesModalComponent {
+
+}
