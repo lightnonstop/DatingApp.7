7b85de02e8033edcedc73f0cdab9265a433957f6
lightnonstop <umichaeledet003@gmail.com>
2023-09-25 19:23:51 +0100

    add get users with their roles method

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/_services/admin.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
-  styleUrls: ['./user-management.component.css']
+  styleUrls: ['./user-management.component.css'],
 })
-export class UserManagementComponent {
+export class UserManagementComponent implements OnInit {
+  users: User[] = [];
 
+  constructor(private adminService: AdminService) {}
+
+  ngOnInit(): void {
+    this.getUsersWithRoles();
+  }
+
+  getUsersWithRoles() {
+    this.adminService.getUsersWithRoles().subscribe({
+      next: (users) => {
+        this.users = users;
+      },
+    });
+  }
 }
