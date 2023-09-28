15e3b357dcda43f9d62145ba8d58907caa008499
lightnonstop <umichaeledet003@gmail.com>
2023-09-28 16:57:09 +0100

    remove trailing spaces

--- API/SignalR/PresenceHub.cs
+++ API/SignalR/PresenceHub.cs
@@ -7,12 +7,12 @@ namespace API.SignalR
     [Authorize]
     public class PresenceHub : Hub
     {
-             private readonly PresenceTracker _tracker;
+        private readonly PresenceTracker _tracker;
 
-             public PresenceHub(PresenceTracker tracker)
-             {
-                 _tracker = tracker;
-             }
+        public PresenceHub(PresenceTracker tracker)
+        {
+            _tracker = tracker;
+        }
         public override async Task OnConnectedAsync()
         {
             await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
