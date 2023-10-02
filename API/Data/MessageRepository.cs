6f0b3d8c0e481e4eaa9ee9f38a47dda294d61a30
lightnonstop <umichaeledet003@gmail.com>
2023-10-02 07:16:22 +0100

    remove message repository's save all method and implementation

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -109,10 +109,5 @@ namespace API.Data
         {
             _context.Connections.Remove(connection);
         }
-
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
-        }
     }
 }
