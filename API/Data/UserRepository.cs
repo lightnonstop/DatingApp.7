4df339983b6220ed297a64be13734a4731d1914f
lightnonstop <umichaeledet003@gmail.com>
2023-10-04 05:45:25 +0100

    update method with parameters

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -18,12 +18,15 @@ namespace API.Data
             _context = context;
             _mapper = mapper;
         }
-        public async Task<MemberDto> GetMemberAsync(string username)
+        public async Task<MemberDto> GetMemberAsync(string username, bool isCurrentUser)
         {
-            return await _context.Users
-                .Where(u => u.UserName == username)
-                .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-                .SingleOrDefaultAsync();
+            var query = _context.Users
+               .Where(u => u.UserName == username)
+               .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+               .AsQueryable();
+
+            if (isCurrentUser) query = query.IgnoreQueryFilters();
+            return await query.FirstOrDefaultAsync();
         }
         public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
         {
