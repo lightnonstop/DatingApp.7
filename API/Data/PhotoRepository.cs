cdb232a33dbb79a6b38662334adaf7341b778704
lightnonstop <umichaeledet003@gmail.com>
2023-10-04 07:48:58 +0100

    add photo reposotory

---
+++ API/Data/PhotoRepository.cs
@@ -0,0 +1,40 @@
+using System.Collections.Immutable;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class PhotoRepository : IPhotoRepository
+    {
+        private readonly DataContext _context;
+        public PhotoRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<Photo> GetPhotoById(int id)
+        {
+            return await _context.Photos.FindAsync(id);
+            // return await _context.Photos
+            //     .IgnoreQueryFilters()
+            //     .SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<PhotoForApprovalDto>> GetUnapprovedPhotos()
+        {
+            return await _context.Photos.Where(p => p.isApproved == false).Select(u => new PhotoForApprovalDto
+            {
+                Id = u.Id,
+                Url = u.Url,
+                Username = u.AppUser.UserName,
+                isApproved = u.isApproved
+            }).ToListAsync();
+        }
+
+        public void RemovePhoto(Photo photo)
+        {
+            _context.Photos.Remove(photo);
+        }
+    }
+}
